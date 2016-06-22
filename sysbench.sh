0535756709b4f0a23f89ba025bdc6d48c056996b
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-06-22 11:50:07 -0400

    log results to separate files

--- sysbench.sh
+++ sysbench.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sysbench --test=cpu --cpu-max-prime=20000 --num-threads=2 run > /results/sysbench-small.log
-sysbench --test=cpu --cpu-max-prime=80000 --num-threads=2 run > /results/sysbench-large.log
+sysbench --test=cpu --cpu-max-prime=20000 --num-threads=2 run > /results/sysbench-cpu-small.log
+sysbench --test=cpu --cpu-max-prime=80000 --num-threads=2 run > /results/sysbench-cpu-large.log
 sysbench --test=fileio --file-total-size=5G prepare
-sysbench --test=fileio --file-total-size=5G --file-test-mode=rndrd --init-rng=on --max-time=300 --max-requests=0 run
-sysbench --test=fileio --file-total-size=5G --file-test-mode=rndwr --init-rng=on --max-time=300 --max-requests=0 run
-sysbench --test=fileio --file-total-size=5G --file-test-mode=seqwr --init-rng=on --max-time=300 --max-requests=0 run
-sysbench --test=fileio --file-total-size=5G --file-test-mode=seqrd --init-rng=on --max-time=300 --max-requests=0 run
+sysbench --test=fileio --file-total-size=5G --file-test-mode=rndrd --init-rng=on --max-time=300 --max-requests=0 run > /results/sysbench-file-randread.log
+sysbench --test=fileio --file-total-size=5G --file-test-mode=rndwr --init-rng=on --max-time=300 --max-requests=0 run > /results/sysbench-file-randwrite.log
+sysbench --test=fileio --file-total-size=5G --file-test-mode=seqwr --init-rng=on --max-time=300 --max-requests=0 run > /results/sysbench-file-seqwrite.log
+sysbench --test=fileio --file-total-size=5G --file-test-mode=seqrd --init-rng=on --max-time=300 --max-requests=0 run > /results/sysbench-file-seqread.log
 sysbench --test=fileio --file-total-size=5G cleanup
