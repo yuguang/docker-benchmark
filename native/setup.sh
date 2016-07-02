398d6d9efa27287de7667a3934ad289387a893c7
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-07-01 21:32:51 -0400

    use emerge to install tools

--- native/setup.sh
+++ native/setup.sh
@@ -6,20 +6,7 @@ cd bonnie++-1.03e
 make
 make install
 
-if ((1<<32)); then
-    sudo apt-get remove iperf3 libiperf0
-    wget https://iperf.fr/download/ubuntu/libiperf0_3.1.2-1_amd64.deb
-    wget https://iperf.fr/download/ubuntu/iperf3_3.1.2-1_amd64.deb
-    sudo dpkg -i libiperf0_3.1.2-1_amd64.deb iperf3_3.1.2-1_amd64.deb
-    rm libiperf0_3.1.2-1_amd64.deb iperf3_3.1.2-1_amd64.deb
-else
-    sudo apt-get remove iperf3 libiperf0
-    wget https://iperf.fr/download/ubuntu/libiperf0_3.1.2-1_i386.deb
-    wget https://iperf.fr/download/ubuntu/iperf3_3.1.2-1_i386.deb
-    sudo dpkg -i libiperf0_3.1.2-1_i386.deb iperf3_3.1.2-1_i386.deb
-    rm libiperf0_3.1.2-1_i386.deb iperf3_3.1.2-1_i386.deb
-fi
-sudo apt-get install sysbench
+emerge iperf sysbench
 
 mkdir /results
 chmod -R 777 /results
